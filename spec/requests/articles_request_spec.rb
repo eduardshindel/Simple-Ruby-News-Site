6ee7e7c1253cebb354643e7766a5d4c1ae49e39e
NikitaIVT <nikita-polishhuk@mail.ru>
2022-01-12 20:54:30 +0700

    small test

--- spec/requests/articles_request_spec.rb
+++ spec/requests/articles_request_spec.rb
@@ -3,8 +3,13 @@ require 'rails_helper'
 RSpec.describe 'Articles requests', type: :request do
     it 'creates new article' do
         User.create(name: 'admin', password: '21232f297a57a5a743894a0e4a801fc3' )
+
+        post "/login",
+            params: { session: { username: "admin", password: "admin" } }
         
-          expect(response.status).to eq(201)
+        post "/articles",
+            params: { article: { title: "Test title", body: "test body text", preview: "test prew", category: "turtle", img: "" }}
+        expect(response.status).to eq(302)
 
     end
     it 'reaches article list' do
