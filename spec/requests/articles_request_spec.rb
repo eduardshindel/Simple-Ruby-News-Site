7d69e727d8076f0a0b28cdf8a1bd097f93204fb0
eduardshindel <shindeleduard10@gmail.com>
2022-01-12 21:51:50 +0700

    Rubocop autofix

--- spec/requests/articles_request_spec.rb
+++ spec/requests/articles_request_spec.rb
@@ -1,86 +1,92 @@
 require 'rails_helper'
 
 RSpec.describe 'Articles requests', type: :request do
+  it 'creates new article' do
+    User.create(name: 'admin', password: '21232f297a57a5a743894a0e4a801fc3')
 
-    it 'creates new article' do
-        User.create(name: 'admin', password: '21232f297a57a5a743894a0e4a801fc3' )
+    post '/login', params: { session: { username: 'admin', password: 'admin' } }
 
-        post "/login",
-            params: { session: { username: "admin", password: "admin" } }
-        
-        post "/articles",
-            params: { article: { title: "Test title", body: "test body text", preview: "test prew", category: "turtle", img: "" }}
-        expect(response.status).to eq(302)
-    end
+    post '/articles',
+         params: { article: { title: 'Test title', body: 'test body text', preview: 'test prew', category: 'turtle',
+                              img: '' } }
+    expect(response.status).to eq(302)
+  end
 
-    it 'creates invalid article' do
-        User.create(name: 'admin', password: '21232f297a57a5a743894a0e4a801fc3' )
+  it 'creates invalid article' do
+    User.create(name: 'admin', password: '21232f297a57a5a743894a0e4a801fc3')
 
-        post "/login",
-            params: { session: { username: "admin", password: "admin" } }
-        
-        post "/articles",
-            params: { article: { title: "Test title", body: "test body text", preview: "test prew", category: "", img: "" }}
-        expect(response.status).to eq(200)
-    end
+    post '/login',
+         params: { session: { username: 'admin', password: 'admin' } }
 
-    it 'visit article create without login' do        
-        post "/articles",
-            params: { article: { title: "Test title", body: "test body text", preview: "test prew", category: "", img: "" }}
-        expect(response.status).to eq(200)
-    end
+    post '/articles',
+         params: { article: { title: 'Test title', body: 'test body text', preview: 'test prew', category: '',
+                              img: '' } }
+    expect(response.status).to eq(200)
+  end
 
-    it 'creates invalid article' do
-        get "/articles/new"
-        expect(response.status).to eq(302)
-    end
+  it 'visit article create without login' do
+    post '/articles',
+         params: { article: { title: 'Test title', body: 'test body text', preview: 'test prew', category: '',
+                              img: '' } }
+    expect(response.status).to eq(200)
+  end
 
-    it 'reaches article list' do
-      get '/articles'
-      expect(response.status).to eq(200)
-    end
+  it 'creates invalid article' do
+    get '/articles/new'
+    expect(response.status).to eq(302)
+  end
 
-    it 'update article' do
-        User.create(name: 'admin', password: '21232f297a57a5a743894a0e4a801fc3' )
+  it 'reaches article list' do
+    get '/articles'
+    expect(response.status).to eq(200)
+  end
 
-        post "/login",
-            params: { session: { username: "admin", password: "admin" } }
-        
-        post "/articles",
-            params: { article: { title: "Test title", body: "test body text", preview: "test prew", category: "turtle", img: "" }}
+  it 'update article' do
+    User.create(name: 'admin', password: '21232f297a57a5a743894a0e4a801fc3')
 
-        patch "/articles/3",
-            params: { article: { title: "Test title", body: "test body text", preview: "test prew", category: "turtle", img: "" }}
+    post '/login',
+         params: { session: { username: 'admin', password: 'admin' } }
 
-        expect(response.status).to eq(302)
-    end
+    post '/articles',
+         params: { article: { title: 'Test title', body: 'test body text', preview: 'test prew', category: 'turtle',
+                              img: '' } }
 
-    it 'wrong update article' do
-        User.create(name: 'admin', password: '21232f297a57a5a743894a0e4a801fc3' )
+    patch '/articles/3',
+          params: { article: { title: 'Test title', body: 'test body text', preview: 'test prew', category: 'turtle',
+                               img: '' } }
 
-        post "/login",
-            params: { session: { username: "admin", password: "admin" } }
-        
-        post "/articles",
-            params: { article: { title: "Test title", body: "test body text", preview: "test prew", category: "turtle", img: "" }}
+    expect(response.status).to eq(302)
+  end
 
-        patch "/articles/4",
-            params: { article: { title: "Test title", body: "test body text", preview: "test prew", category: "", img: "" }}
+  it 'wrong update article' do
+    User.create(name: 'admin', password: '21232f297a57a5a743894a0e4a801fc3')
 
-        expect(response.status).to eq(200)
-    end
+    post '/login',
+         params: { session: { username: 'admin', password: 'admin' } }
 
-    it 'destroy article' do
-        User.create(name: 'admin', password: '21232f297a57a5a743894a0e4a801fc3' )
+    post '/articles',
+         params: { article: { title: 'Test title', body: 'test body text', preview: 'test prew', category: 'turtle',
+                              img: '' } }
 
-        post "/login",
-            params: { session: { username: "admin", password: "admin" } }
-        
-        post "/articles",
-            params: { article: { title: "Test title", body: "test body text", preview: "test prew", category: "turtle", img: "" }}
+    patch '/articles/4',
+          params: { article: { title: 'Test title', body: 'test body text', preview: 'test prew', category: '',
+                               img: '' } }
 
-        delete "/articles/5"
+    expect(response.status).to eq(200)
+  end
 
-        expect(response.status).to eq(302)
-    end
+  it 'destroy article' do
+    User.create(name: 'admin', password: '21232f297a57a5a743894a0e4a801fc3')
+
+    post '/login',
+         params: { session: { username: 'admin', password: 'admin' } }
+
+    post '/articles',
+         params: { article: { title: 'Test title', body: 'test body text', preview: 'test prew', category: 'turtle',
+                              img: '' } }
+
+    delete '/articles/5'
+
+    expect(response.status).to eq(302)
+  end
 end
