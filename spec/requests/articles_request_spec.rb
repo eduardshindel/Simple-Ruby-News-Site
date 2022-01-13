a73443e29311cedb925431cbb5c5823d351a78ab
NikitaIVT <nikita-polishhuk@mail.ru>
2022-01-13 15:52:48 +0700

    tests rework

--- spec/requests/articles_request_spec.rb
+++ spec/requests/articles_request_spec.rb
@@ -1,38 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe 'Articles requests', type: :request do
-
-    it 'creates new article' do
+    before(:all) do
         User.create(name: 'admin', password: '21232f297a57a5a743894a0e4a801fc3' )
 
         post "/login",
             params: { session: { username: "admin", password: "admin" } }
-        
+    end
+
+    it 'creates new article' do
         post "/articles",
             params: { article: { title: "Test title", body: "test body text", preview: "test prew", category: "turtle", img: "" }}
         expect(response.status).to eq(302)
     end
 
-    it 'creates invalid article' do
-        User.create(name: 'admin', password: '21232f297a57a5a743894a0e4a801fc3' )
-
-        post "/login",
-            params: { session: { username: "admin", password: "admin" } }
-        
+    it 'creates invalid article' do        
         post "/articles",
             params: { article: { title: "Test title", body: "test body text", preview: "test prew", category: "", img: "" }}
         expect(response.status).to eq(200)
     end
 
-    it 'visit article create without login' do        
-        post "/articles",
-            params: { article: { title: "Test title", body: "test body text", preview: "test prew", category: "", img: "" }}
-        expect(response.status).to eq(200)
-    end
-
-    it 'creates invalid article' do
+    it 'go to articles/new' do
         get "/articles/new"
-        expect(response.status).to eq(302)
+        expect(response.status).to eq(200)
     end
 
     it 'reaches article list' do
@@ -40,12 +30,7 @@ RSpec.describe 'Articles requests', type: :request do
       expect(response.status).to eq(200)
     end
 
-    it 'update article' do
-        User.create(name: 'admin', password: '21232f297a57a5a743894a0e4a801fc3' )
-
-        post "/login",
-            params: { session: { username: "admin", password: "admin" } }
-        
+    it 'update article' do    
         post "/articles",
             params: { article: { title: "Test title", body: "test body text", preview: "test prew", category: "turtle", img: "" }}
 
@@ -56,11 +41,6 @@ RSpec.describe 'Articles requests', type: :request do
     end
 
     it 'wrong update article' do
-        User.create(name: 'admin', password: '21232f297a57a5a743894a0e4a801fc3' )
-
-        post "/login",
-            params: { session: { username: "admin", password: "admin" } }
-        
         post "/articles",
             params: { article: { title: "Test title", body: "test body text", preview: "test prew", category: "turtle", img: "" }}
 
@@ -71,16 +51,18 @@ RSpec.describe 'Articles requests', type: :request do
     end
 
     it 'destroy article' do
-        User.create(name: 'admin', password: '21232f297a57a5a743894a0e4a801fc3' )
-
-        post "/login",
-            params: { session: { username: "admin", password: "admin" } }
-        
         post "/articles",
             params: { article: { title: "Test title", body: "test body text", preview: "test prew", category: "turtle", img: "" }}
 
         delete "/articles/5"
-
         expect(response.status).to eq(302)
+    end
+
+    describe 'visit articles/new without login' do
+        it 'visiting should be failed' do        
+            post "/articles",
+                params: { article: { title: "Test title", body: "test body text", preview: "test prew", category: "", img: "" }}
+            expect(response.status).to eq(200)
+        end
     end
 end
