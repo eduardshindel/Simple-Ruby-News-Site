5e9344f668189e28e8736b3d32738f4849178e09
lew14748 <dimabdb@yandex.ru>
2022-01-13 20:08:03 +0600

    Finally v1.0 (+rubocop_fix)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
   def not_found
-    raise ActionController::RoutingError.new('Not Found')
+    raise ActionController::RoutingError, 'Not Found'
   end
-  
 end
