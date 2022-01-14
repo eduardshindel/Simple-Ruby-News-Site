0624bad0ea7bf53599ef63c69535a2efed581887
eduardshindel <shindeleduard10@gmail.com>
2022-01-14 21:59:53 +0700

    Version 1.0 completed!

--- spec/requests/mascot_request_spec.rb
+++ spec/requests/mascot_request_spec.rb
@@ -1,17 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe "Mascots", type: :request do
-    context 'when visiting our mascot page' do
-    
-        it 'visits mascots page' do
-          get '/mascot'
-          expect(response.status).to eq(200)
-        end
-        
-        it 'visits mascots page in a wrong way' do
+RSpec.describe 'Mascots', type: :request do
+  context 'when visiting our mascot page' do
+    it 'visits mascots page' do
+      get '/mascot'
+      expect(response.status).to eq(200)
+    end
 
-          
-          expect{get '/mascot/totally_wrong_url'}.to raise_error(ActionController::RoutingError)
-        end
-      end
+    it 'visits mascots page in a wrong way' do
+      expect { get '/mascot/totally_wrong_url' }.to raise_error(ActionController::RoutingError)
+    end
+  end
 end
